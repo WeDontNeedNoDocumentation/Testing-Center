8441372f61b90fd3d237dc533201d350020fb8b5
danharel <cheezzywizz@gmail.com>
2015-10-13 03:40:32 -0400

    Working on Instructor.

--- Testing-Center/src/Instructor.java
+++ Testing-Center/src/Instructor.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.joda.time.DateTime;
+
 /**
  * 
  */
@@ -11,6 +17,7 @@ public class Instructor {
 	private String name;
 	private String email;
 	private TestingCenter tC;
+	private String instructorId;
 	
 	/**
 	 * 
@@ -22,12 +29,41 @@ public class Instructor {
 		this.tC = tC;
 	}
 	
-	public void viewExams() {
+	public List<Exam> viewExams() {
+		List<Exam> exams = new ArrayList<Exam>();
+		String queryString = String.format("SELECT * FROM exam "
+				+ "INNER JOIN instructor ON exam.%s=instructor.%s",
+				this.instructorId,
+				this.instructorId
+				);
+		Database db = Database.getDatabase();
+		List<Map<String,Object>> examList = db.query(queryString);
+		for (Map<String,Object> exam : examList) {
+			System.out.println(exam);
+			
+			String examId = (String) exam.get("examId");
+			DateTime start = new DateTime((int) exam.get("start")*1000);
+			DateTime end = new DateTime((int) exam.get("end")*1000);
+			
+			Exam newExam = new Exam(examId, start, end);
+			appointments.add(newExam);
+		}
 		
+		return exams;
 	}
 	
-	public void makeExam() {
-		
+	public void makeExam(Exam exam, DateTime start, DateTime end, boolean courseExam, String status) {
+		String queryString = String.format("INSERT INTO exam VALUES ("
+				+ "'%s', %d, %d, %d, '%s', '%s')", 
+				exam.getId(), 
+				start.getMillis()/1000,
+				end.getMillis()/1000,
+				courseExam ? 0 : 1,
+				status,
+				this.instructorId
+				);
+		Database db = Database.getDatabase();
+		db.updateQuery(queryString);
 	}
 	
 	public void viewAvailability() {
