eef291cefa5a8bf0769a36b5fd1f5774bd8a9cdb
danharel <cheezzywizz@gmail.com>
2015-10-12 21:51:10 -0400

    Got email working. 4 points!

--- Testing-Center/src/Exam.java
+++ Testing-Center/src/Exam.java
@@ -7,8 +7,24 @@ public class Exam {
 	private String examID;
 	private DateTime start;
 	private DateTime end;
+	
 	public Exam() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Exam(String examID, DateTime start, DateTime end) {
+		super();
+		this.examID = examID;
+		this.start = start;
+		this.end = end;
+	}
+
+	public DateTime getStart() {
+		return start;
+	}
+	
+	public DateTime getEnd() {
+		return end;
 	}
 
 }
